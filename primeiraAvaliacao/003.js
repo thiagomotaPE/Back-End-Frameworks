2a5ed2b83e659fb07a6aa1fb6355b0358aca9b28
mota <thiagomotape@hotmail.com.br>
2023-03-24 17:38:11 -0300

    Update 003.js
    melhorando a validação de dados

--- primeiraAvaliacao/003.js
+++ primeiraAvaliacao/003.js
@@ -1,14 +1,17 @@
 const readline = require('readline-sync');
 
-const pessoa = readline.question("Ola, você é estudante ou idoso(a)? S/N ").toUpperCase();
+let pessoa = "";
+
+do {
+    console.log("Digite uma entrada valida (S/N)!")
+    pessoa = readline.question("Ola, voce e estudante ou idoso(a)? S/N ").toUpperCase();
+}while(pessoa !== "S" && pessoa !== "N")
 
 switch (pessoa) {
     case "S":
         console.log("Você tem direito a meia entrada");
-        break
+        break;
     case "N":
         console.log("Você não tem direito a meia entrada");
-        break
-    default:
-        console.log("entrada invalida!");
+        break;
 }
