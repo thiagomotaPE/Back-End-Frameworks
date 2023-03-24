58204160abb4d4cf6ec87b2a65fdd0e4a4b995d9
mota <thiagomotape@hotmail.com.br>
2023-03-24 17:56:10 -0300

    Update 005.js
    melhorando a validação do 005

--- primeiraAvaliacao/005.js
+++ primeiraAvaliacao/005.js
@@ -1,8 +1,23 @@
 const readline = require('readline-sync');
 
-const num1 = Number(readline.question("Digite um numero: "));
-const num2 = Number(readline.question("Digite outro numero: "));
-const num3 = Number(readline.question("Digite outro numero: "));
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+
+do {
+    console.log("Digite um numero e nao uma letra!")
+    num1 = Number(readline.question("Digite um numero: "));
+}while(isNaN(num1))
+
+do {
+    console.log("Digite um numero e nao uma letra!")
+    num2 = Number(readline.question("Digite outro numero: "));
+}while(isNaN(num2))
+
+do {
+    console.log("Digite um numero e nao uma letra!")
+    num3 = Number(readline.question("Digite o ultimo numero: "));
+}while(isNaN(num3))
 
 let max = Math.max(num1, num2, num3);
 console.log(max);
