ba66c3b5c6878f06a1e4f0bf8fa5a5e677412e85
mota <thiagomotape@hotmail.com.br>
2023-03-29 19:40:30 -0300

    Update 005.js

--- primeiraAvaliacao/005.js
+++ primeiraAvaliacao/005.js
@@ -5,17 +5,14 @@ let num2 = 0;
 let num3 = 0;
 
 do {
-    console.log("Digite um numero e nao uma letra!")
     num1 = Number(readline.question("Digite um numero: "));
 }while(isNaN(num1))
 
 do {
-    console.log("Digite um numero e nao uma letra!")
     num2 = Number(readline.question("Digite outro numero: "));
 }while(isNaN(num2))
 
 do {
-    console.log("Digite um numero e nao uma letra!")
     num3 = Number(readline.question("Digite o ultimo numero: "));
 }while(isNaN(num3))
 
