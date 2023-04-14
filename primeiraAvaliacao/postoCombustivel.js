ad1fdb4d601cde27573ec6e20462880cfac4f526
mota <thiagomotape@hotmail.com.br>
2023-04-14 17:56:34 -0300

    Update postoCombustivel.js

--- primeiraAvaliacao/postoCombustivel.js
+++ primeiraAvaliacao/postoCombustivel.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync")
 
   combustivel = readline.question('Escolha o tipo do combustivel digite G-Gasolina e A-Álcool: ')
-  litro = Number(readline.question('Quantos litros de combusrivel voce quer?'))
+  ltr = Number(readline.question('Quantos litros de combusrivel voce quer?'))
   let valorDesconto
 
     if(combustivel == 'g'){
@@ -9,18 +9,18 @@ const readline = require("readline-sync")
         let valor = litro * 5
         let desconto = (4 * valor)/100
         valorDesconto = valor - desconto
-      } else if(litro > 20){
-        let valor = litro * 5
+      } else if(ltr > 20){
+        let valor = ltr * 5
         let desconto = (6 * valor)/100
         valorDesconto = valor - desconto
       }
     }else if(combustivel == 'a'){
-      if(litro <= 20){
-        let valor = litro * 4
+      if(ltr <= 20){
+        let valor = ltr * 4
         let desconto = (3 * valor)/100
         valorDesconto = valor - desconto
-      } else if(litro > 20){
-        let valor = litro * 4
+      } else if(ltr > 20){
+        let valor = ltr * 4
         let desconto = (5 * valor)/100
         valorDesconto = valor - desconto
       }
