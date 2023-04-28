070bce59cd352d080e755a4ce3f87b0d848a186f
mota <thiagomotape@hotmail.com.br>
2023-04-28 20:44:28 -0300

    Create teoria.js
    aprendendo sobre classes e objetos

---
+++ pooJS/teoria.js
@@ -0,0 +1,30 @@
+//criando uma classe no nodejs
+class fruta {
+
+    // no javascript os atributos podem ser declarador no metodo construtor
+
+    //função construtor
+    constructor(cor, sabor, formato, tem_semente, tamanho, quantidade) { 
+        //cor = dado que pegou da classe main   |   this.cor = variavel da classe
+        this.cor = cor 
+        this.sabor = sabor
+        this.formato = formato
+        this.tem_semente = tem_semente
+        this.tamanho = tamanho
+        this.quantidade = quantidade
+    }
+
+    venderFruta(qtd) {
+        return this.quantidade -= qtd
+    }
+
+    reporFruta(qtd) {
+        return this.quantidade += qtd
+    }
+}
+
+let banana = new fruta("vermelha", "aguada", "cilindrico", false, "18 cm", 57)
+console.log(banana)
+
+console.log(banana.venderFruta(3))
+console.log(banana)
