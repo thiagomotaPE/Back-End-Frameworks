e0f250cff4f235de40cc7fcdb0946537017b011e
mota <thiagomotape@hotmail.com.br>
2023-05-12 21:11:35 -0300

    update

--- jsonJS/desafio.js
+++ jsonJS/desafio.js
@@ -1,53 +1,45 @@
 class Barraca {
-    constructor(frutas, quantidade, preco, descricao){
-      this.frutas = frutas
-      this.quantidade = quantidade
-      this.preco = preco
-      this.descricao = descricao
-      this.frutas = frutas
-      this.preco = preco
-      this.faturamento = 0 
-    }
-  
-    vender(fruta, quantidade) {
-      
-      switch(fruta){
-        case 'Maçã':
-          if (quantidade > this.quantidade[0]) {
-            console.log("Não há maçãs no estoque.")
-          } else {
-            this.quantidade[0] = this.quantidade[0] - quantidade
-            this.faturamento += quantidade * preco[0]
-            console.log(this.quantidade[0])
-            return this.faturamento  
-          }
-          break
-        case 'Banana':
-          if (quantidade > this.quantidade[1]) {
-            console.log("Não há bananas vermelhas no estoque.")
-          } else {
-            this.quantidade[1] = this.quantidade[1] - quantidade
-            this.faturamento += quantidade * preco[1]
-            return this.faturamento
-          }
-          break
-            
-        default:
-          break
+  constructor(estoque) {
+    this.estoque = estoque
+    this.faturamento = 0
+  }
+
+  vender(fruta, quantidade) {
+    for (fruta of this.estoque) {
+      if (fruta.nome == 'Maçã') {
+        novaFruta = {
+          "nome": fruta.nome,
+          "quantidade": fruta.quantidade - quantidade,
+          "preco": fruta.preco,
+          "descricao": fruta.descricao
+        }
+        // Continue esse código! 
       }
     }
-  
-    getFaturamento() {
-      return this.faturamento
-    }
-    
+
+  }
+
+  getFaturamento() {
+    return this.faturamento
+  }
+
 }
-  
-  frutas = ["Maçã", "Banana"]
-  quantidade = [20, 20]
-  preco = [2.00, 7.00]
-  descricao = ["Maçã da branca de neve", "banana vermelha de xablau"]
-  const barraca_tinoco = new Barraca(frutas, quantidade, preco, descricao)
-  
-  barraca_tinoco.vender("Maçã", Number(10))
-  console.log(`Parabéns, você vendeu a Maçã. Faturado: R$: ${barraca_tinoco.getFaturamento()}`)
+
+estoque = [
+  {
+    "nome": "Maçã",
+    "quantidade": 20,
+    "preco": 2.00,
+    "descricao": "Maçã vermelha"
+  },
+  {
+    "nome": "Banana",
+    "quantidade": 10,
+    "preco": 7.00,
+    "descricao": "Banana vermelha"
+  }
+]
+const barraca_tinoco = new Barraca(estoque)
+
+barraca_tinoco.vender("Maçã", Number(10))
+console.log(`Parabéns, você vendeu a Maçã. Faturado: R$: ${barraca_tinoco.getFaturamento()}`)
---
+++ jsonJS/teoria.js
@@ -0,0 +1,14 @@
+estoque = [
+    {
+        "nome": "maça",
+        "quantidade": 10,
+        "preco": 2.00,
+        "descricao": "boa"
+    },
+    {
+        "nome": "banana",
+        "quantidade": 20,
+        "preco": 5.00,
+        "descricao": "vermelha"
+    }
+]
